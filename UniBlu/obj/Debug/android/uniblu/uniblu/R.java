663938996a227038f97c8253f38bb7aa549a4213
James L Bergeron <jberg475@mtroyal.ca>
2016-10-14 21:08:35 -0600

    design

--- UniBlu/obj/Debug/android/uniblu/uniblu/R.java
+++ UniBlu/obj/Debug/android/uniblu/uniblu/R.java
@@ -21,17 +21,18 @@ public final class R {
         public static final int uniblu=0x7f020004;
     }
     public static final class id {
-        public static final int announcementButton=0x7f070002;
-        public static final int coursePlannerButton=0x7f070001;
-        public static final int imageView1=0x7f070000;
-        public static final int instructorBiosButton=0x7f070003;
-        public static final int labSchedulesButton=0x7f070004;
-        public static final int loginButton=0x7f07000a;
-        public static final int passwordEditText=0x7f070009;
-        public static final int studentSocietyButton=0x7f070005;
-        public static final int textView1=0x7f070007;
-        public static final int toolbar=0x7f070006;
-        public static final int usernameEditText=0x7f070008;
+        public static final int announcementButton=0x7f070003;
+        public static final int coursePlannerButton=0x7f070002;
+        public static final int imageView1=0x7f070001;
+        public static final int instructorBiosButton=0x7f070004;
+        public static final int labSchedulesButton=0x7f070005;
+        public static final int loginButton=0x7f07000b;
+        public static final int passwordEditText=0x7f07000a;
+        public static final int studentSocietyButton=0x7f070006;
+        public static final int textView1=0x7f070008;
+        public static final int toolbar=0x7f070007;
+        public static final int toolbar1=0x7f070000;
+        public static final int usernameEditText=0x7f070009;
     }
     public static final class layout {
         public static final int landingpageview=0x7f030000;
