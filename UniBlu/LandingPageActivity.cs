be5ebe8515f6b99ac0abea7393735b3bd2010b34
spitrola <spitrola27@gmail.com>
2016-10-17 20:32:05 -0600

    added toolbar to landing page - fix for white text color

--- UniBlu/LandingPageActivity.cs
+++ UniBlu/LandingPageActivity.cs
@@ -19,42 +19,42 @@ namespace UniBlu
 		{
 			base.OnCreate(savedInstanceState);
 			SetContentView(Resource.Layout.LandingPageView);
-			var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar1);
+			var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
 			SetActionBar(toolbar);
 			ActionBar.Title = GetString(Resource.String.appName);
 			FindViews();
 			HandleEvents();
 		}
-        protected override void OnResume()
-        {
-            base.OnResume();
-            Music.play(this, Resource.Raw.pomp_loop);
-        }
-        protected override void OnPause()
-        {
-            base.OnPause();
-            Music.stop(this);
-        }
+		protected override void OnResume()
+		{
+			base.OnResume();
+			Music.play(this, Resource.Raw.pomp_loop);
+		}
+		protected override void OnPause()
+		{
+			base.OnPause();
+			Music.stop(this);
+		}
 
-        public override bool OnCreateOptionsMenu(IMenu menu)
-        {
-            MenuInflater.Inflate(Resource.Menu.SettingsMenu, menu);
-            return base.OnCreateOptionsMenu(menu);
-        }
+		public override bool OnCreateOptionsMenu(IMenu menu)
+		{
+			MenuInflater.Inflate(Resource.Menu.SettingsMenu, menu);
+			return base.OnCreateOptionsMenu(menu);
+		}
 
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            switch (item.ItemId)
-            {
-                case Resource.Id.settings:
-                    var intent = new Intent(this, typeof(SettingsActivity));
-                    StartActivity(intent);
-                    return true;
-            }
-            return base.OnOptionsItemSelected(item);
-        }
+		public override bool OnOptionsItemSelected(IMenuItem item)
+		{
+			switch (item.ItemId)
+			{
+				case Resource.Id.settings:
+					var intent = new Intent(this, typeof(SettingsActivity));
+					StartActivity(intent);
+					return true;
+			}
+			return base.OnOptionsItemSelected(item);
+		}
 
-        private void FindViews()
+		private void FindViews()
 		{
 
 		}
