6baff932a7fba5788353d44bc206f8c2c13a61e0
James L Bergeron <jberg475@mtroyal.ca>
2016-10-20 19:53:18 -0600

    the files for about .... sighs

---
+++ UniBlu/AboutActivity.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace UniBlu
+{
+    [Activity(Label = "About Us")]
+    public class AboutActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.aboutLayout);
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetActionBar(toolbar);
+            ActionBar.Title = GetString(Resource.String.aboutUs);
+            FindViews();
+            HandleEvents();
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Music.play(this, Resource.Raw.pomp_loop);
+        }
+        protected override void OnPause()
+        {
+            base.OnPause();
+            Music.stop(this);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.SettingsMenu, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.settings:
+                    var intent = new Intent(this, typeof(SettingsActivity));
+                    StartActivity(intent);
+                    return true;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FindViews()
+        {
+
+        }
+        private void HandleEvents()
+        {
+
+        }
+    }
+}
