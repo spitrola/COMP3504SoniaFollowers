b2daa85e69d6bfe0cedf664b2a1be251671d4868
Ryan Barton <ry-guy-17@hotmail.com>
2016-11-16 11:20:17 -0700

    updated courses model

--- UniBlu/Model/Course.cs
+++ UniBlu/Model/Course.cs
@@ -15,14 +15,15 @@ namespace UniBlu.Model
     public class Course
     {
         public int CourseId { get; set; }             // Auto incremented by the database
-        public int CRN { get; set; }            // i.e., some 5 digit number
+        public int SectionId { get; set; }            // i.e., some 5 digit number
         public string Subject { get; set; }     // i.e., COMP
         public int CourseNumber { get; set; }   // i.e., 1501
-        public int Section { get; set; }        // i.e., 001
+        //public int Section { get; set; }        // i.e., 001
         public string Title { get; set; }       // i.e., Introduction to Programming
         public string [][] days { get; set; }       // i.e., TR (Tuesday and Thursday) with start & end time 9 and 10:20
         public string Instructor { get; set; }  // i.e., Jordan Kidney
         public string Location { get; set; }    // i.e., B215
-        public bool FullClass { get; set; }     // i.e., true = full, false = room available
+        //todo check for full feature class
+        //public bool FullClass { get; set; }     // i.e., true = full, false = room available
     }
 }
