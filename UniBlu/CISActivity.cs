0671ffd7737df772bbd3f9502af8242c92b51e57
spitrola <spitrola27@gmail.com>
2016-11-17 16:45:16 -0700

    changed Web Site & Face Book to CIS3 and Facebook - student society page tabs

--- UniBlu/CISActivity.cs
+++ UniBlu/CISActivity.cs
@@ -14,46 +14,46 @@ using UniBlu.Fragments;
 
 namespace UniBlu
 {
-    [Activity(Label = "CIS Activity", Theme = "@style/TabTheme")]
-    public class CISActivity : TabBaseActivity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-            SetContentView(Resource.Layout.CISLayout);
-            SetToolBar();
-        }
+	[Activity(Label = "CIS Activity", Theme = "@style/TabTheme")]
+	public class CISActivity : TabBaseActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.CISLayout);
+			SetToolBar();
+		}
 
-        private void SetToolBar()
-        {
-            this.ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
-            ActionBar.Title = GetString(Resource.String.cisTitle);
+		private void SetToolBar()
+		{
+			this.ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+			ActionBar.Title = GetString(Resource.String.cisTitle);
 
-            AddTab("Web Site", new CISWebSiteFragment());
-            AddTab("Face Book", new CISFaceBookFragment());
-        }
+			AddTab("CIS3", new CISWebSiteFragment());
+			AddTab("Facebook", new CISFaceBookFragment());
+		}
 
-        private void AddTab(string tabText, Fragment view)
-        {
-            var tab = this.ActionBar.NewTab();
-            tab.SetText(tabText);
+		private void AddTab(string tabText, Fragment view)
+		{
+			var tab = this.ActionBar.NewTab();
+			tab.SetText(tabText);
 
-            tab.TabSelected += delegate (object sender, ActionBar.TabEventArgs e)
-            {
-                var fragment = this.FragmentManager.FindFragmentById(Resource.Id.CISFragmentContainer);
-                if (fragment != null)
-                {
-                    e.FragmentTransaction.Remove(fragment);
-                }
-                e.FragmentTransaction.Add(Resource.Id.CISFragmentContainer, view);
-            };
+			tab.TabSelected += delegate (object sender, ActionBar.TabEventArgs e)
+			{
+				var fragment = this.FragmentManager.FindFragmentById(Resource.Id.CISFragmentContainer);
+				if (fragment != null)
+				{
+					e.FragmentTransaction.Remove(fragment);
+				}
+				e.FragmentTransaction.Add(Resource.Id.CISFragmentContainer, view);
+			};
 
-            tab.TabUnselected += delegate (object sender, ActionBar.TabEventArgs e)
-            {
-                e.FragmentTransaction.Remove(view);
-            };
-            // Add tab to action bar tab collection
-            this.ActionBar.AddTab(tab);
-        }
-    }
+			tab.TabUnselected += delegate (object sender, ActionBar.TabEventArgs e)
+			{
+				e.FragmentTransaction.Remove(view);
+			};
+			// Add tab to action bar tab collection
+			this.ActionBar.AddTab(tab);
+		}
+	}
 }
